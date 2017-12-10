759f537f17aa2788c6ed65316a94329f967aa023
wanyanxx <33455930+wanyanxx@users.noreply.github.com>
2017-12-10 13:40:40 +0800

    添加更多的猫

--- js/app.js
+++ js/app.js
@@ -1,7 +1,39 @@
-var Cat=function(){
-    this.clickCount=ko.observable(0);
-    this.name=ko.observable('Tommy');
-    this.imgSrc=ko.observable('img/1.jpg');
+var initialCats=[
+    {
+        clickCount:0,
+        name:'Tommy',
+        imgSrc:'img/1.jpg',
+        nickNames:['Abby','Mr-T'],
+    },
+    {
+        clickCount:0,
+        name:'Habby',
+        imgSrc:'img/2.jpg',
+        nickNames:['Baby'],    
+    },
+    {
+        clickCount:0,
+        name:'Elle',
+        imgSrc:'img/3.jpg',
+        nickNames:['Hello'],    
+    },
+    {
+        clickCount:0,
+        name:'Selly',
+        imgSrc:'img/4.jpg',
+        nickNames:['Sweet'],   
+    },
+    {
+        clickCount:0,
+        name:'Jane',
+        imgSrc:'img/5.jpg',
+        nickNames:['Mimi'],    
+    },
+]
+var Cat=function(data){
+    this.clickCount=ko.observable(data.clickCount);
+    this.name=ko.observable(data.name);
+    this.imgSrc=ko.observable(data.imgSrc);
     this.level=ko.computed(function(){
         var title;
         var  clickCount = this.clickCount();
@@ -10,18 +42,22 @@ var Cat=function(){
             else if(clickCount<30){return "Old";}
         return title;  
     },this);
-    this.nameList=ko.observable('NickName');
-    this.nickNames=ko.observableArray([
-        {name:'Abby'},
-        {name:'Baby'},
-        {name:'Canny'},
-    ])
+   
+    this.nickNames = data.nickNames;
 }
+
 var ViewModel=function(){
     var self = this;
-    this.currentCat=ko.observable(new Cat());
+    this.catList =ko.observableArray([]);
+    initialCats.forEach(function(catItem){
+        self.catList.push(new Cat(catItem));
+    })
+    this.currentCat=ko.observable(this.catList()[0]);
     this.incrementCounter=function(){
     self.currentCat().clickCount(self.currentCat().clickCount()+1);
+    }
+    this.changeCurrentCat=function(currentCat){
+        self.currentCat(currentCat);
     }
 }
 ko.applyBindings(new ViewModel());
